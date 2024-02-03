911b62bcab67b8c24c1ecc4cdb55da967f883242
apsogood <aaran.patel18@gmail.com>
2024-02-03 17:56:09 -0500

    moved folder

---
+++ subtraction.py
@@ -0,0 +1,8 @@
+print("Welcome to my subtraction calculator. I will make more calculators in the furture. Please consider giving my code a star I spent a while to make these mini projects!")
+
+first_answer = float(input("What do you want your starting number?"))
+second_answer = float(input("How much do you want to subtract from your first answer?"))
+
+sum = first_answer - second_answer
+
+print(first_answer + " - " + second_answer + " = " + sum)
