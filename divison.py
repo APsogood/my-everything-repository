a22f81211d4b65d6170fabadce745263b398ab3a
apsogood <aaran.patel18@gmail.com>
2024-02-03 14:06:31 -0500

    divison.py

---
+++ divison.py
@@ -0,0 +1,9 @@
+print("This is my divison calculator. Please give me a star since I spent a long time for this. Thank you for using my calculator.\n")
+
+first_answer = float(input("What do you want your first number to be?\n"))
+second_answer = float(input("Now, what do you want to divide with your first answer?"))
+
+sum = first_answer / second_answer
+print(str(first_answer) + " / " + str(second_answer) + " = " + str(sum))
+
+# I hope you enjoyed my divison calculator! Leave a star if you enjoyed this!
