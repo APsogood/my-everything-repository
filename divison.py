2fa2a9e2d9a7f39d1bdd9de47d442a709e7cdaf5
apsogood <aaran.patel18@gmail.com>
2024-02-03 18:29:40 -0500

    divison.py

--- divison.py
+++ divison.py
@@ -1,7 +1,7 @@
-print("This is my divison calculator. Please give me a star since I spent a long time for this. Thank you for using my calculator.\n")
+print("This is my divison calculator. Please give me a star and follow me since I spent a long time for this. Thank you for using my calcul-ator.\n")
 
 first_answer = float(input("What do you want your first number to be?\n"))
-second_answer = float(input("Now, what do you want to divide with your first answer?"))
+second_answer = float(input("Now, what do you want to divide with your first answer?\n"))
 
 sum = first_answer / second_answer
 print(str(first_answer) + " / " + str(second_answer) + " = " + str(sum))
