52235b248fd8e82bd340e76e774a4d698a1803f0
apsogood <aaran.patel18@gmail.com>
2024-02-03 18:25:47 -0500

    mutiplication.py

---
+++ mutiplication.py
@@ -0,0 +1,10 @@
+print("This is my mutiplication calculator. Please give me a star since I spent a while for these projects. Thank you for using my ca-lculator.\n")
+
+first_answer = float(input("What is your first number you want to add?\n"))
+second_answer = float(input("What is your second number to mutiply with your first answer?\n"))
+
+sum = first_answer * second_answer
+
+print(str(first_answer) + " x " + str(second_answer) + " = " + str(sum))
+
+# Please give this code a star I spent a while doing these mini projects!
