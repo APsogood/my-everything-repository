febc04e64c6348e8b6df429d18416ae8ddf24120
apsogood <aaran.patel18@gmail.com>
2024-02-04 13:26:37 -0500

    guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -3,14 +3,11 @@ print("This is a guessing game and you need to guess the secret number! Are you 
 import random
 
 def guessing_game():
-    # Generate a random number between 1 and 10
     secret_number = random.randint(1, 10)
     
-    # Initialize the number of attempts
     attempts = 0
     
     
-    # Loop until the user guesses the correct number
     while True:
         guess = int(input("Guess a number between 1 and 10: \n"))
         attempts += 1
