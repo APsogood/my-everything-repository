6c1023c0ed82fac8262fdfa5fc4a86bcbc8d31a3
apsogood <aaran.patel18@gmail.com>
2024-02-04 13:39:23 -0500

    weight converter

--- weight-converter.py
+++ weight-converter.py
@@ -1,10 +1,12 @@
-print("This is a weight converted. I took some time on this project so please give this a star and follow me!\n")
+print("This is a weight converter. I took some time on this project so please give this a star and follow me! \n")
 
-weight = float(input("What is your weight? "))
-unit = input('Do you want your weight converted in (L)bs or (K)gs? ')
+weight = float(input("What is your weight? \n"))
+unit = input('Do you want your weight converted in (L)bs or (K)gs? \n')
 if unit.upper() == "L":
     converted = weight * 0.45
-    print(f"You are {converted} kilos.\n")
+    print(f"You are {converted} kilos. \n")
 else:
     converted = weight / 0.45
-    print(f"You are {converted} pounds.\n")
+    print(f"You are {converted} pounds. \n")
+
+print("Thanks for using my weight converter and please give this code a star and follow me. Have a great day! \n")
