b31b5d85d5e51a7e3399304b70739c308a482384
apsogood <aaran.patel18@gmail.com>
2024-02-04 13:33:55 -0500

    mutiplication calculator

--- mutiplication.py
+++ mutiplication.py
@@ -1,4 +1,4 @@
-print("This is my mutiplication calculator. Please give me a star and follow me because I spent a while for these projects for you guys. Tha-nk you for using my calculator. ")
+print("This is my mutiplication calculator. Please give me a star and follow me because I spent a while for these projects for you guys. Tha-nk you for using my calculator.\n")
 
 first_answer = float(input("What is your first number you want to add? "))
 second_answer = float(input("What is your second number to mutiply with your first answer? "))
