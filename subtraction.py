8f44cefd81770bb968493dd24bba3f368021c345
apsogood <aaran.patel18@gmail.com>
2024-02-04 13:24:57 -0500

    subtraction calculator

--- subtraction.py
+++ subtraction.py
@@ -1,10 +1,10 @@
-print("Welcome to my subtraction calculator. I am in the process of making more calculators. Please give a star, I spend a while to m-ake for you guys to enjoy.\n")
+print("Welcome to my subtraction calculator. I am in the process of making more calculators. Please give a star, I spend a while to m-ake for you guys to enjoy. ")
 
-first_answer = float(input("What do you want your starting number?\n"))
-second_answer = float(input("How much do you want to subtract from your first answer?\n"))
+first_answer = float(input("What do you want your starting number? "))
+second_answer = float(input("How much do you want to subtract from your first answer? "))
 
 result = first_answer - second_answer
 
 print(str(first_answer) + " - " + str(second_answer) + " = " + str(result))
 
-# I hope you enjoyed my divison calculator! Leave a star if you enjoyed this!
+# I hope you enjoyed my divison calculator! Leave a star and follow me if you enjoyed this!
