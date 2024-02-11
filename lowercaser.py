eaf24d73ad85a1d4751442199991925470386f3e
apsogood <aaran.patel18@gmail.com>
2024-02-11 14:47:11 -0500

    lowercaser

---
+++ lowercaser.py
@@ -0,0 +1,3 @@
+time_to_lowercase = input("Input whatever you want and it will be returned lowercased! Please follow me and give me a star! ")
+lowercased = time_to_lowercase.lower()
+print(lowercased)
