def5df707d0dc7d9d10033e0284f0be276c19d6c
apsogood <aaran.patel18@gmail.com>
2024-02-15 20:28:18 -0500

    minor checker

---
+++ python/minor-checker.py
@@ -0,0 +1,7 @@
+# Follow me and give my repository a star! I apperciate if you do :)
+age = int(input("How old are you? "))
+if age < 18:
+    print("Your a minor. Wait until your 18 and you will not be a minor!")
+else:
+    print("You are not a minor.")
+# I hope you enjoy my code. Bye!
