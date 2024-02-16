15d8236d3b40e43a922766da419701f9c8d65348
apsogood <aaran.patel18@gmail.com>
2024-02-15 20:31:44 -0500

    capitalizer

--- python/capitalizer.py
+++ python/capitalizer.py
@@ -1,3 +1,4 @@
+# Leave this code a like please follow me too! I spend a while doing these projects.
 time_to_capital = input("Input whatever you want and it will be returned capital! Please follow me and give me a star and enjoy! ")
 myword = time_to_capital.upper()
 print("It's capitalized. Enjoy! " + myword)
