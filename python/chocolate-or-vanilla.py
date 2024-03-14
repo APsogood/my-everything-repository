7492453c97528a17b07465d5b297ad661361fc2c
APSogood <aaran.patel18@gmail.com>
2024-03-14 15:31:39 -0400

    chocolate or vanilla ice cream question

--- python/chocolate-or-vanilla.py
+++ python/chocolate-or-vanilla.py
@@ -1,10 +1,10 @@
 while True:
-    question = input("What ice cream flavor do you prefer? Vanilla or Chocolate?")
+    question = input("What ice cream flavor do you prefer? Vanilla or Chocolate?\n")
     if question.lower() == "vanilla":
-        print("Good choice. Vanilla ice cream is really good!")
+        print("Good choice. Vanilla ice cream is really good!\n")
         break
     elif question.lower() == "chocolate":
-        print("Chocolate. You can never go wrong with chocolate. Good choice!")
+        print("Chocolate. You can never go wrong with chocolate. Good choice!\n")
         break
     else:
-        print(f"{question} was not a proper answer. Let me ask again.")
+        print(f"{question} was not a proper answer. Let me ask again.\n")
