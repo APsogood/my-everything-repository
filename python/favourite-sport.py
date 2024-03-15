d066b86e9a686c5bb2eca9bb06a5e8a68a9e92e4
APSogood <aaran.patel18@gmail.com>
2024-03-15 11:52:14 -0400

    favourite sport

--- python/favourite-sport.py
+++ python/favourite-sport.py
@@ -4,11 +4,11 @@ while True:
         outdoor_or_indoor = input("'Indoor soccer' or 'outdoor soccer'? ").lower()
         if outdoor_or_indoor == 'indoor soccer':
             name = input("What is your name? ")
-            print(name + ' likes ' + outdoor_or_indoor + '.')
+            print(name.capitalize + ' likes ' + outdoor_or_indoor + '.')
             break
         elif outdoor_or_indoor == 'outdoor soccer':
             name_two = input("What is your name? ")
-            print(name_two + " likes " + outdoor_or_indoor)
+            print(name_two.capitalize + " likes " + outdoor_or_indoor)
             break
         else:
             print(f"Invalid choice: {outdoor_or_indoor}. Please choose 'Indoor soccer' or 'Outdoor soccer'.")
@@ -16,11 +16,11 @@ while True:
         outdoor_or_indoor_two = input("Do you prefer 'outdoor basketball' or 'indoor basketball'? ").lower()
         if outdoor_or_indoor_two == "outdoor basketball":
             name_three = input("What is your name? ")
-            print(name_three + " likes " + outdoor_or_indoor_two + '.')
+            print(name_three.capitalize + " likes " + outdoor_or_indoor_two + '.')
             break
         elif outdoor_or_indoor_two == "indoor basketball":
             name_four = input("What is your name? ")
-            print(name_four + " likes " + outdoor_or_indoor_two)
+            print(name_four.capitalize + " likes " + outdoor_or_indoor_two)
             break
         else:
             print(f"Invalid choice: {outdoor_or_indoor_two}. Please choose 'Indoor basketball' or 'Outdoor basketball'.")
