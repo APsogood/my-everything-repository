4b31450e8bf47d2a28ab0fe083d7534cfc6c35f3
APSogood <aaran.patel18@gmail.com>
2024-03-21 10:35:37 -0400

    tic tac toe (still in progress)

--- python/tic-tac-toe.py
+++ python/tic-tac-toe.py
@@ -1,11 +1,64 @@
 import tkinter as tk
 import random
 
-def next_turn():
-    pass
+def next_turn(row, column):
+    
+    global player
+
+    if buttons[row][column]['text'] == "" and check_winner() is False:
+
+        if player == players[0]:
+
+            buttons[row][column]['text'] = player
+
+            if check_winner() is False:
+                players = players[1]
+                label.config(text=(players[1]+" turn"))
+
+            elif check_winner() is True:
+                label.config(text=(players[0]+" wins"))
+
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"))
+
+        else:
+
+            buttons[row][column]['text'] = player
+
+            if check_winner() is False:
+                players = players[0]
+                label.config(text=(players[0]+" turn"))
+
+            elif check_winner() is True:
+                label.config(text=(players[1]+" wins"))
+
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"))
+
 
 def check_winner():
-    pass
+    
+    for row in range(3):
+        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
+            return True
+    
+    for column in range(3):
+        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            return True
+        
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
+        return True
+    
+    elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+        return True
+    
+    elif empty_spaces() is False:
+        return "Tie"
+    
+    else:
+        return False
+    
+    
 
 def empty_spaces():
     pass
