5f6d4e20f10aaf5cce096ad8902d57ab5efa652c
APSogood <aaran.patel18@gmail.com>
2024-03-26 19:43:08 -0400

    weight

--- python/weight.py
+++ python/weight.py
@@ -1,6 +1,6 @@
 weight_input = input("Enter the weight of a 1 to 10-year-old kid: ")
 
-if weight_input.isdigit():  # Check if input consists of only digits
+if weight_input.isdigit():
     weight = int(weight_input)
     if weight > 10:
         print(f"{weight} is too high of a number.")
@@ -9,24 +9,24 @@ if weight_input.isdigit():  # Check if input consists of only digits
     else:
         print(f"{weight} is within the valid range.")
         if weight == 1:
-            print("An average weight for boys is 9.6 kgs or 21 pounds. For girls, an average weight is 8.9 kgs or 19 pounds.")
+            print("For boys: 9.6 kgs (21 pounds), For girls: 8.9 kgs (19 pounds)")
         elif weight == 2:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For boys: 11 to 14 kgs (24.25 to 30.8 pounds), For girls: 10 to 13 kgs (22 to 28.6 pounds)")
         elif weight == 3:
-            print("For an average boy they should weight 15 to 17 kgs or 33 to 37.5 pounds")
+            print("For boys: 15 to 17 kgs (33 to 37.5 pounds), For girls: 14 to 16 kgs (30.8 to 35.2 pounds)")
         elif weight == 4:
-            print("For an average boy they should weight 16 to 19 kgs or 35.5 to 42 pounds")
+            print("For boys: 16 to 19 kgs (35.2 to 42 pounds), For girls: 15 to 18 kgs (33 to 39.6 pounds)")
         elif weight == 5:
-            print("For an average boy they should weight 18 to 22 kgs or 39.5 to 48.5 pounds")
+            print("For boys: 18 to 22 kgs (39.6 to 48.5 pounds), For girls: 17 to 21 kgs (37.4 to 46.2 pounds)")
         elif weight == 6:
-            print("For an average boy they should weight 20 to 25 kgs or 44 to 55 pounds")
+            print("For boys: 20 to 25 kgs (44 to 55 pounds), For girls: 19 to 24 kgs (41.8 to 52.8 pounds)")
         elif weight == 7:
-            print("For an average boy they should weight 22 to 27 kgs or 48.5 to 59.5 pounds")
+            print("For boys: 22 to 27 kgs (48.5 to 59.5 pounds), For girls: 21 to 26 kgs (46.2 to 57.2 pounds)")
         elif weight == 8:
-            print("For an average boy they should weight 24 to 29 kgs or 53 to 64 pounds")
+            print("For boys: 24 to 29 kgs (53 to 64 pounds), For girls: 23 to 28 kgs (50.6 to 61.6 pounds)")
         elif weight == 9:
-            print("For an average boy they should weight 26 to 31 kgs or 57 to 68.5 pounds")
+            print("For boys: 26 to 31 kgs (57.2 to 68.5 pounds), For girls: 25 to 30 kgs (55 to 66 pounds)")
         elif weight == 10:
-            print("For an average boy they should weight 27 to 33 kgs or 59.5 to 73 pounds")
+            print("For boys: 27 to 33 kgs (59.5 to 73 pounds), For girls: 26 to 32 kgs (57.2 to 70.4 pounds)")
 else:
     print(f"'{weight_input}' is not a valid answer.")
