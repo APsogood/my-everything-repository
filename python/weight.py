296732cc78da8b48041b21d47d36ce06551e0638
APSogood <aaran.patel18@gmail.com>
2024-03-26 19:40:54 -0400

    weight

--- python/weight.py
+++ python/weight.py
@@ -13,20 +13,20 @@ if weight_input.isdigit():  # Check if input consists of only digits
         elif weight == 2:
             print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
         elif weight == 3:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 15 to 17 kgs or 33 to 37.5 pounds")
         elif weight == 4:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 16 to 19 kgs or 35.5 to 42 pounds")
         elif weight == 5:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 18 to 22 kgs or 39.5 to 48.5 pounds")
         elif weight == 6:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 20 to 25 kgs or 44 to 55 pounds")
         elif weight == 7:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 22 to 27 kgs or 48.5 to 59.5 pounds")
         elif weight == 8:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 24 to 29 kgs or 53 to 64 pounds")
         elif weight == 9:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 26 to 31 kgs or 57 to 68.5 pounds")
         elif weight == 10:
-            print("For an average boy they should weight 11 to 14 kgs or 21 33.5 pounds")
+            print("For an average boy they should weight 27 to 33 kgs or 59.5 to 73 pounds")
 else:
     print(f"'{weight_input}' is not a valid answer.")
