98ee28b218c8f0c76592f342f4a3661ebbe9db15
APSogood <aaran.patel18@gmail.com>
2024-04-01 20:44:06 -0400

    palindrome checker

---
+++ python/palindrome-checker.py
@@ -0,0 +1,3 @@
+reverse = input("This code reverses your text. Put something in and it will reverse it! ")
+reverse = ''.join(reversed(e))
+print("There, I've reversed it here is the output: " + reverse)
