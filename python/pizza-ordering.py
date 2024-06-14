39a67ded318d817114b70c991973ccfccc2e949c
APSogood <aaran.patel18@gmail.com>
2024-06-14 14:48:24 -0400

    pizza ordering

--- python/pizza-ordering.py
+++ python/pizza-ordering.py
@@ -1,6 +1,22 @@
 import time
+
 while True:
-    question = input("What pizza restaurant are you ordering? ")
-    call = print('Calling...')
+    question = input("What pizza restaurant are you ordering from? ")
+    
+    print('Calling...')
     time.sleep(3)
-    greeting = input
+    
+    while True:
+        greeting = input("Hello! Welcome to " + question + ". What pizza would you like to order: 'cheese' or 'pepperoni'? ")
+        
+        if greeting.lower() == "cheese":
+            print("Thank you for placing your order. Come to your nearest " + question + " store and pay for your pizza!")
+            break
+        elif greeting.lower() == "pepperoni":
+            print("Thank you for placing your order. Come to your nearest " + question + " store and pay for your pizza!")
+            break
+        else:
+            print("Invalid choice. Please choose either 'cheese' or 'pepperoni'.")
+
+    break
+
