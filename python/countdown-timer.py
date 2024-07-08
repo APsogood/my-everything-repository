888106a56d9b863aafe173d412b4be73006a5c39
APSogood <aaran.patel18@gmail.com>
2024-07-08 17:40:56 -0400

    countdown timer

---
+++ python/countdown-timer.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+seconds = int(input("Enter the time in seconds: "))
+countdown_timer(seconds)
