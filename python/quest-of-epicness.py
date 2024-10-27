8d6ee9618f4b47dc225c08997ce2ab3620a7ce28
APSogood <aaran.patel18@gmail.com>
2024-10-27 18:14:15 -0400

    quest of epicness (UPDATED!)

--- python/quest-of-epicness.py
+++ python/quest-of-epicness.py
@@ -11,7 +11,11 @@ def adventure_game():
     elif decide == "find out what it is":
         print("An old man gave you a treasure chest! Rest to be continued...")
     elif choice == "right":
-        print("You walked right. Rest to be continued.")
+        eat = ("You walked right. You found a fruit. This one seems different... Eat or throw away?")
+        if eat == "eat":
+            print("Your body feels different... To be continued...")
+        elif eat == "throw away":
+            print("The fruit got thrown away. An old man said those can go around for 350,000,000 dollars and if consumed it is said to give you special super-human powers.")
     else:
         print("Invalid choice. You wander around and get eaten by a giant bird. You lose!")
 
