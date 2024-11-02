6fbb2fbf65d57375b8b8ba0cc92b7ee32e9b56b3
APSogood <aaran.patel18@gmail.com>
2024-11-02 17:06:06 -0400

    make mario part 2

---
+++ python/make-mario-2.py
@@ -0,0 +1,64 @@
+p1 = "             *  *"
+p2 = "            **  **  "
+p3 = "           ***  *** "
+p4 = "          ****  **** "
+p5 = "         *****  ***** "
+p6 = "        ******  ******"
+p7 = "       *******  *******"
+p8 = "      ********  ********"
+p9 = "     *********  *********"
+p10 = "    **********  **********"
+p11 = "   ***********  ***********"
+p12 = "  ************  ************"
+
+number = input("Welcome to make mario 2! We've extended the number by 9-12. Now, choose a number by 9-12. ")
+
+if number == "9":
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    print(p7)
+    print(p8)
+    print(p9)
+
+elif number == "10":
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    print(p7)
+    print(p8)
+    print(p9)
+    print(p10)
+
+elif number == "11":
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    print(p7)
+    print(p8)
+    print(p9)
+    print(p10)
+    print(p11)
+
+elif number == "12":
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    print(p7)
+    print(p8)
+    print(p9)
+    print(p10)
+    print(p11)
+    print(p12)
