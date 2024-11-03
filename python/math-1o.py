413b841ed82c1eb169776a1e5e87ed61c700370b
APSogood <aaran.patel18@gmail.com>
2024-11-03 15:35:18 -0500

    math 1o (still in progress and unplayable right now)

---
+++ python/math-1o.py
@@ -0,0 +1,8 @@
+help = input("Welcome to math 1o! We are still developing our math questions. Need some help? Type in (math 1o help) for help! NOTE THAT THIS CODE IS STILL BEING PROGRAMMED! Game is not playable yet.")
+
+if help == "math 1o help":
+    print("""1 - You will have math questions
+             2 - Solve the math questions
+             3 - You have to type the math questions in.
+             3 - Try to get 10/10 questions correct. Good luck!""")
+    
