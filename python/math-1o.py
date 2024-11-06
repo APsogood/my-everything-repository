3d316012f55f972fd9329f3f5513233f10ae885f
APSogood <aaran.patel18@gmail.com>
2024-11-05 19:41:40 -0500

    math 1o (currently under developent)

--- python/math-1o.py
+++ python/math-1o.py
@@ -1,4 +1,4 @@
-help = input("Welcome to math 1o! We are still developing our math questions. Need some help? Type in (math 1o help) for help! To play, type in (math questions) to play our math game! NOTE THAT THIS CODE IS STILL BEING PROGRAMMED! Game is not playable yet.")
+help = input("Welcome to math 1o! We are still developing our math questions. Need some help? Type in (math 1o help) for help! To play, type in (math questions) to answer the math questions! NOTE THAT THIS CODE IS STILL BEING PROGRAMMED! Game is not playable yet. ")
 
 if help == "math 1o help":
     print("""1 - You will have math questions
@@ -8,14 +8,24 @@ if help == "math 1o help":
 if help == "math questions":
     math_operations = input("What operation would you like to do (addition) or (subtraction)? ")
 if math_operations == "addition":
-    addition_yes_or_no = input("Are you sure you would like to addition? (yes) or (no)")
+    addition_yes_or_no = input("Are you sure you would like to addition? (yes) or (no) ")
     if addition_yes_or_no == "yes":
-        pass
-    if addition_yes_or_no == "no":
-        pass
+        questions = input("What is 35+35?")
+        
+    
+    elif addition_yes_or_no == "no":
+        print("As you wish.")
+        quit
+    
+    
+    
+    
     elif math_operations == "subtraction":
-        subtraction_yes_or_no = input("Are you sure you would like to do subtraction? (yes) or (no)")
+        subtraction_yes_or_no = input("Are you sure you would like to do subtraction? (yes) or (no) ")
         if subtraction_yes_or_no == "yes":
             pass
         elif subtraction_yes_or_no == "no":
-            pass
+         print("As you wish.")
+         quit
+    
+         
