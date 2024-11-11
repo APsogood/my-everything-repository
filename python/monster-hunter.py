75cf0b61b562ac3467ca87536d6ebe99fdade03f
APSogood <aaran.patel18@gmail.com>
2024-11-10 20:37:29 -0500

    monster hunter (not playable yet)

--- python/monster-hunter.py
+++ python/monster-hunter.py
@@ -1,1 +1,6 @@
-welcome = input("Welcome to monster hunter!")
+import random
+import time
+word_tuple = ("Monster", "Destroyer", "Monkey", "Marathon", "Fisherman", "Friendly", "Demolish", "Saskatchewan", "Television", "Giraffe", "Kangaroo", "Fighter Plane", "Flag Football", "Tetanus")
+welcome = input("Welcome to Monster Hunter! In this game, you need to type in a word within a certain amount of time. There are 3 difficulties. Easy, Medium and Hard. Type in (tutorial) to play the tutorial!")
+if welcome == "tutorial".lower():
+    pass
