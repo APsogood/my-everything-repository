f18a3fe1ed0c896e3f47b10897f8fd9ea59d699c
APSogood <aaran.patel18@gmail.com>
2024-11-11 20:16:21 -0500

    monster hunter (game unfinished)

--- python/monster-hunter.py
+++ python/monster-hunter.py
@@ -1,6 +1,12 @@
 import random
 import time
-word_tuple = ("Monster", "Destroyer", "Monkey", "Marathon", "Fisherman", "Friendly", "Demolish", "Saskatchewan", "Television", "Giraffe", "Kangaroo", "Fighter Plane", "Flag Football", "Tetanus")
-welcome = input("Welcome to Monster Hunter! In this game, you need to type in a word within a certain amount of time. There are 3 difficulties. Easy, Medium and Hard. Type in (tutorial) to play the tutorial!")
+word_tupe_easy = ("When", "Master", "Done", "Happy", "Help", "Find", "Read", "Feed", "March", "Fruit", "Simple", "Sharp", "Sheep", "Sleep", "Upset", "Then", "Runner", "Jump", "Rabbit", "Great", "Happy", )
+word_tuple_medium = ("Monster", "Destroyer", "Monkey", "Marathon", "Fisherman", "Friendly", "Demolish", "Saskatchewan", "Television", "Giraffe", "Kangaroo", "Fighter Plane", "Flag Football", "Tetanus", "Excuses", "Married", "Explosion")
+word_tuple_hard = ("Discombobulated", "Confusion", "Arachnophobia", "Megalophobia", "Microphobia", "Teraphobia", "Nyctophobia", "Hematophobia", "Aibohphobia" "")
+word_tuple_extreme = ("Hippopotomonstrosesquippedaliophobia", "Lymphangioleiomyomatosis", "Supercalifragilisticexpialidocious")
+word_tuple_unbeatable = ("Pneumonoultramicroscopicsilicovolcanoconiosis")
+
+
+welcome = input("Welcome to Monster Hunter! In this game, you need to type in a word within a certain amount of time. There are 3 difficulties. Easy, Medium and Hard. Type in (tutorial) to play the tutorial! ")
 if welcome == "tutorial".lower():
     pass
