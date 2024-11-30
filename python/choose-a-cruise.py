907ced70989585b3af7365effa998dc83d0b2d1f
APSogood <aaran.patel18@gmail.com>
2024-11-30 18:17:00 -0500

    choose a cruise

---
+++ python/choose-a-cruise.py
@@ -0,0 +1,24 @@
+entry = input("Welcome to Choose A Cruise! Before we get started, what cruise line do you want to choose (MSC), (Royal Caribbean), (Virgin Voyages), (Carnival Cruises), (Norwegian Cruises), or (Disney Cruises)? ").lower()
+
+if entry == "msc":
+    people = input(int("How many people are coming with you? "))
+
+elif entry == "virgin voyages":
+    people = input(int("How many people are coming with you? "))
+
+elif entry == "carnival cruises":
+    people = input(int("How many people are coming with you? "))
+
+elif entry == "norwegian cruises":
+    people = input(int("How many people are coming with you? "))
+
+elif entry == "disney cruises":
+    people = input(int("How many people are coming with you? "))
+else:
+    print("Invalid option.")
+
+
+
+
+
+
