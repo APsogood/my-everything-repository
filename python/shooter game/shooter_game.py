cd7002669fd97e9aa13a8d52e3c778d12e76110a
“APsogood” <aaran.patel18@gmail.com>
2025-03-03 18:51:06 -0500

    shooter game (game is complete!)

--- python/shooter game/shooter_game.py
+++ python/shooter game/shooter_game.py
@@ -42,15 +42,15 @@ grenade_thrown = False
 
 
 # Load music and sounds
-#pygame.mixer.music.load("audio/music.mp3")
-#pygame.mixer.music.set_volume(0.3)
-#pygame.mixer.music.play(-1, 0.0, 5000)
+pygame.mixer.music.load("audio/music.mp3")
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1, 0.0, 5000)
 jump_fx = pygame.mixer.Sound("audio/jump.wav")
-jump_fx.set_volume(0.05)
+jump_fx.set_volume(0.5)
 shot_fx = pygame.mixer.Sound("audio/shot.wav")
-shot_fx.set_volume(0.05)
+shot_fx.set_volume(0.5)
 grenade_fx = pygame.mixer.Sound("audio/grenade.wav")
-grenade_fx.set_volume(0.05)
+grenade_fx.set_volume(0.5)
 
 
 # Load images
