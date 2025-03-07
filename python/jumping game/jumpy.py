fc1586853f3c0bda23492d0c74a7435bae1bc7cd
“APsogood” <aaran.patel18@gmail.com>
2025-03-06 20:44:04 -0500

    jumpy 3 and 4/15

--- python/jumping game/jumpy.py
+++ python/jumping game/jumpy.py
@@ -12,6 +12,13 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Jumpy")
 
+#set frame rate
+clock = pygame.time.Clock()
+FPS = 60
+
+#game variables
+GRAVITY = 1
+
 #define colours
 WHITE = (255, 255, 255)
 
@@ -28,9 +35,46 @@ class Player():
         self.height = 40
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x, y)
+        self.vel_y = 0
+        self.flip = False
+
+    def move(self):
+        #reset variables
+        dx = 0
+        dy = 0
+
+        #process keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+           dx = -10
+           self.flip = True
+        if key[pygame.K_d]:
+           dx = 10
+           self.flip = False
+
+        #gravity
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+        #ensure player doesn't go off the edge of the screen
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > SCREEN_WIDTH:
+            dx = SCREEN_WIDTH - self.rect.right
+
+        
+        #check collision with ground
+        if self.rect.bottom + dy > SCREEN_HEIGHT:
+            dy = 0
+            self.vel_y = -20
+
+
+        #update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
 
     def draw(self):
-        screen.blit(self.image, (self.rect.x - 10, self.rect.y - 5))
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), (self.rect.x - 10, self.rect.y - 5))
         pygame.draw.rect(screen, WHITE, self.rect, 2)
 
 
@@ -40,23 +84,22 @@ jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 run = True
 while run:
 
+    clock.tick(FPS)
+
+    jumpy.move()
+
     #draw background
     screen.blit(bg_image, (0, 0))
 
     #draw sprites
     jumpy.draw()
 
-
-
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-
     #update display
     pygame.display.update()
-    
-
 
 pygame.quit()
