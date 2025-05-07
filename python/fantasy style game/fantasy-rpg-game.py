1078753288544cb4a56e07b46296cd1933a2f34f
“APsogood” <aaran.patel18@gmail.com>
2025-05-06 21:17:22 -0400

    fantasy rpg game 3/8

--- python/fantasy style game/fantasy-rpg-game.py
+++ python/fantasy style game/fantasy-rpg-game.py
@@ -42,10 +42,43 @@ class Fighter():
         self.start_potions = potions
         self.potions = potions
         self.alive = True
-        img = pygame.image.load(f"img/{self.name}/Idle/0.png").convert_alpha()
-        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0#0:idle, 1:attack, 2:hurt, #3:dead
+        self.update_time = pygame.time.get_ticks()
+        #load idle images
+        temp_list = []
+        for i in range(8):  
+            img = pygame.image.load(f"img/{self.name}/Idle/{i}.png").convert_alpha()
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load attack images
+        temp_list = []
+        for i in range(8):  
+            img = pygame.image.load(f"img/{self.name}/Attack/{i}.png").convert_alpha()
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+
+    def update(self):
+        animation_cooldown = 100
+        #handle animation
+        #update image
+        self.image = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has run out then reset it back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+
 
 
     def draw(self):
@@ -73,8 +106,10 @@ while run:
     draw_panel()
 
     #draw fighter
+    knight.update()
     knight.draw()
     for bandit in bandit_list:
+        bandit.update()
         bandit.draw()
 
     for event in pygame.event.get():
@@ -84,4 +119,3 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-
